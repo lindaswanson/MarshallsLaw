d3299b558ed26ff18ccf398f83a9d80846843d3a
Linda Swanson <linda.a.swanson@gmail.com>
2025-03-24 13:42:13 -0400

    Initial Commit

---
+++ MarshallsLaw Watch App/MarshallsLawApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshallsLawApp.swift
+//  MarshallsLaw Watch App
+//
+//  Created by Linda Swanson on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MarshallsLaw_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
